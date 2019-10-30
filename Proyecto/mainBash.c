b6d7849ccd2fe03213ce318596b163b66aa1131b
ShurikenX <benitez.emilio.morales@gmail.com>
2019-10-30 08:01:39 -0600

    delCom

--- Proyecto/mainBash.c
+++ Proyecto/mainBash.c
@@ -26,29 +26,3 @@ int main(){
   //ESTO ES UNA  PRUEBA DE MOLINA
   //PRUEBA DE BENITEZ ALV :V
 }
-//si guardo yo, o guardas tú, o guardara crixo, todo me aparece, en este caso, sólo a mí
-//osea que me encargaré de subirlo en un moemnto, como podrás darte cuenta, en tu atom se abrió
-//otra pestaña apra poder estar aquí justo ahora, es verdad??
-//ok, eso es porque esa pestaña es un portal a mi computadora, estas wachando mi computadora
-//cuando suba los cambios y los bajes, podrás verlos en tu propia pestaña...o pestañas
-//y entonces podrás ejecutar loc cambios en tu propia compu
-//este modo de edición en tiempo real es para facilitarnos el editar rápido el PROYECTO
-//por eso mensionaba que sería bueno sólo si nos ponemos de acuerdo los 3...o 2 de nsootros,
-//en caso de que uno no pueda
-//(es importante que inicien sesión de github con el gatito, porque apra ser anfitrión se necesita
-//la cuenta)
-
-
-//
-//otra cosa, para ser anfitrión, ahí mismo en la torre de wifi, en lugar de poner el url,
-//activas el switch qeu está ahí, creoq eu dice "share", entocnes se abrirá
-//un pequeño cuadro con texto, esa amdre es la url como la que yo te rolé para que podamos
-//unirnos a tu compu, siq uieres ahorita probamos...
-//
-//yas
-//podemos proar mañana? simon brou//es que s eva a enfriar mi agua de bañar
-//jsjsjsjs
-
-
-
-//jajaja a webo XD ,simon karnal, nad más subo estos cambios
